99456670ca11fbf173697f649d724105fe36bece
Shareef Hassan <creazyhell@gmail.com>
2025-09-27 13:50:52 +0300

    refactor(composables): add types to isUsingKeyboard

--- src/composables/is-using-keyboard.ts
+++ src/composables/is-using-keyboard.ts
@@ -1,14 +1,15 @@
-import { isClient } from "@/utils"
+import type { Ref } from "vue"
 import { ref } from "vue"
+import { isClient } from "@/utils"
 
-export const isUsingKeyboard = ref(false)
+export const isUsingKeyboard: Ref<boolean> = ref(false)
 
-function onKeydown() {
+function onKeydown(): void {
   isUsingKeyboard.value = true
   document.addEventListener("pointerdown", onPointer, { capture: true, once: true })
 }
 
-function onPointer() {
+function onPointer(): void {
   isUsingKeyboard.value = false
   document.addEventListener("keydown", onKeydown, { capture: true, once: true })
 }
