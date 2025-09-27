d2116bef21c6ac03431a553425caf90c4122a0bd
Shareef Hassan <creazyhell@gmail.com>
2025-09-27 12:05:50 +0300

    skip

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vitest/config" />
-import { fileURLToPath, URL } from "node:url"
+import { URL, fileURLToPath } from "node:url"
 
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
 import { resolve } from "node:path"
-import Unocss from "unocss/vite"
+import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
+import Unocss from "unocss/vite"
+import { defineConfig } from "vite"
 // import { presetIcons } from "@unocss/preset-icons"
-// import DevTools from "vite-plugin-vue-devtools"
+import DevTools from "vite-plugin-vue-devtools"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), Unocss()],
+  plugins: [vue(), vueJsx(), Unocss(), DevTools()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
